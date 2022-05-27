e9672fe50915f6208d6d5944f72e919b1f3edf3b
Rodrigo <rodrigorael53@gmail.com>
2022-05-26 23:44:37 -0300

    creating the countdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+const COUNTDOWN_INITIAL_TIME_IN_SECONDS = 5; // 25 minutes
 
 function App() {
+  const [secondsAmount, setSecondsAmount] = useState(COUNTDOWN_INITIAL_TIME_IN_SECONDS);
+
+  useEffect(() => {
+    if (secondsAmount === 0) {
+      alert('Chegou')
+      return;
+    };
+
+    setTimeout(() => {
+      setSecondsAmount(state => state -1);
+    }, 1000);
+  }, [secondsAmount]);
+
+  const minutes = Math.floor(secondsAmount / 60);
+  const seconds = secondsAmount % 60;
+
   return (
-    <h1>asdafe</h1>
+    <div>
+      <span>{String(minutes).padStart(2, '0')}</span>
+      <span>:</span>
+      <span>{String(seconds).padStart(2, '0')}</span>
+    </div>
   )
 }
 
